9a76a9377e0415fd8895959e280756bdc82041ba
huyphung1602 <huy.phung.sw@gmail.com>
2021-04-03 23:14:59 +0700

    Add derived instances

---
+++ DerivedInstances.hs
@@ -0,0 +1,20 @@
+data Person =
+  Person { firstName :: String
+         , lastName :: String
+         , age :: Int
+         } deriving (Eq)
+
+data Day = Monday 
+         | Tuesday
+         | Wednesday
+         | Thursday
+         | Friday
+         | Saturday
+         | Sunday   
+           deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+
+-- succ Monday
+-- pred Saturday
+-- [Thursday .. Sunday]  
+-- [minBound .. maxBound] :: [Day]
