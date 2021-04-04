a5c521138e8ae27f1f7cd3c4519d5850754f1f9e
huyphung1602 <huy.phung.sw@gmail.com>
2021-04-04 15:44:21 +0700

    Update FunctorAgain code

--- FunctorAgain.hs
+++ FunctorAgain.hs
@@ -3,9 +3,15 @@
 --           putStrLn $ "You said " ++ line ++ " backwards!"
 --           putStrLn $ "Yes, you really said " ++ line ++ " backwards!"
 
-import Data.Char ( toUpper )
-import Data.List ( intersperse )
+-- import Data.Char ( toUpper )
+-- import Data.List ( intersperse )
 
-main :: IO ()
-main = do line <- fmap (intersperse '-' . reverse . map toUpper) getLine
-          putStrLn line
+-- main :: IO ()
+-- main = do line <- fmap (intersperse '-' . reverse . map toUpper) getLine
+--           putStrLn line
+
+data CMaybe a = CNothing | CJust a deriving (Show)
+
+instance Functor CMaybe where
+  fmap f CNothing = CNothing
+  fmap f (CJust x) = CJust (f x)
